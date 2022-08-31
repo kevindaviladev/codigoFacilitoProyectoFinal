2a52ec6b7aae98b861a53eda0f48a25f4cb04699
Kevin Davila <kfariidavila@gmail.com>
2022-08-30 23:36:15 -0500

    first commit - ready to work!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'curso-profesional-angular';
+  imgCody = 'assets/codys/1.png';
+  ngOnInit(){
+    this.getCodyImg();
+  }
+
+  getCodyImg(){
+    let rand = Math.random() * 18;
+    rand = Math.floor(rand);
+    this.imgCody = `assets/codys/${rand}.png`;
+  }
 }
